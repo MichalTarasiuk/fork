23115df5887d4ad9f6a2886fcb68940076d1eb42
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-04-03 22:01:20 +0200

    create omitByType helper

---
+++ src/helpers/omitByValue.helper.ts
@@ -0,0 +1,15 @@
+import { isFunction, isPrimitive } from './helpers'
+
+export const omitByValue = <TObject extends Record<PropertyKey, unknown>>(
+  object: TObject,
+  value: ((item: unknown) => boolean) | unknown
+) =>
+  Object.keys(isPrimitive(object) ? {} : object)
+    .filter((key) =>
+      isFunction(value) ? !value(object[key]) : object[key] !== value
+    )
+    .reduce<Record<PropertyKey, unknown>>((acc, key) => {
+      acc[key] = object[key]
+
+      return acc
+    }, {}) as TObject
---
+++ tests/helpers/omitByValue.helper.test.ts
@@ -0,0 +1,42 @@
+import { omitByValue } from '../../src/helpers/helpers'
+
+describe('omitByValue', () => {
+  it('should return empty object when value is primitive', () => {
+    // @ts-ignore
+    expect(omitByValue(1, null)).toEqual({})
+    // @ts-ignore
+    expect(omitByValue('', null)).toEqual({})
+    // @ts-ignores
+    expect(omitByValue(null, null)).toEqual({})
+    // @ts-ignore
+    expect(omitByValue(undefined, null)).toEqual({})
+    // @ts-ignore
+    expect(omitByValue(false, null)).toEqual({})
+  })
+
+  it(`should pick values which are't 1`, () => {
+    const object = {
+      a: 1,
+      b: 2,
+      c: 3,
+    }
+
+    expect(omitByValue(object, 1)).toEqual({
+      b: 2,
+      c: 3,
+    })
+  })
+
+  it(`should pick values which are't match to the callback`, () => {
+    const object = {
+      a: 1,
+      b: 2,
+      c: 3,
+    }
+
+    expect(omitByValue(object, (item) => item === 1)).toEqual({
+      b: 2,
+      c: 3,
+    })
+  })
+})
