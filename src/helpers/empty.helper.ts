0762e4298cf5e99f0a7fea2bec5fa4d717091f91
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-04-06 19:08:41 +0200

    rewrote empty helper

--- src/helpers/empty.helper.ts
+++ src/helpers/empty.helper.ts
@@ -1,9 +1,19 @@
-export const empty = <TObject extends Record<PropertyKey, unknown>>(
-  object: TObject
-): {} => {
-  Object.keys(object).forEach((key) => {
-    delete object[key]
-  })
+import { isPlainObject } from './helpers'
 
-  return object
+export function empty(value: Record<PropertyKey, unknown>): {}
+export function empty(value: Array<unknown>): []
+
+export function empty<
+  TValue extends Record<PropertyKey, unknown> | Array<unknown>
+>(value: TValue) {
+  if (isPlainObject(value)) {
+    Object.keys(value).forEach((key) => {
+      delete value[key]
+    })
+
+    return value
+  }
+
+  value.length = 0
+  return value
 }
--- tests/helpers/empty.helper.test.ts
+++ tests/helpers/empty.helper.test.ts
@@ -1,7 +1,7 @@
 import { empty } from '../../src/helpers/helpers'
 
 describe('empty', () => {
-  it('should return empty value', () => {
+  it('should return empty object', () => {
     // arrange
     const value = {
       a: 1,
@@ -12,12 +12,28 @@ describe('empty', () => {
     expect(empty(value)).toEqual({})
   })
 
-  it('should return value with the same refference', () => {
+  it('should return object with the same refference', () => {
     // arrange
     const value = {
       a: 1,
       b: 2,
     }
+
+    // assert
+    expect(empty(value)).toBe(value)
+  })
+
+  it('should return empty array', () => {
+    // arrange
+    const value = [1, 2, 3, 4]
+
+    // assert
+    expect(empty(value)).toEqual([])
+  })
+
+  it('should return array with the same refference', () => {
+    // arrange
+    const value = [1, 2, 3, 4]
 
     // assert
     expect(empty(value)).toBe(value)
