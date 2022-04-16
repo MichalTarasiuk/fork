931c03a1313904140f270de35de4e04ed5886b27
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-04-16 10:30:03 +0200

    remove watch option

--- src/factory.types.ts
+++ src/factory.types.ts
@@ -1,7 +1,6 @@
-import type { Selector, SetState, Patch } from './store.types'
+import type { Selector } from './store.types'
 
 export type Config<TState, TSelector extends Selector<TState>> = {
-  watch?: boolean
   equalityFn?: (
     nextState: ReturnType<TSelector>,
     state: ReturnType<TSelector>
@@ -11,7 +10,3 @@ export type StateMap<TState extends Record<PropertyKey, unknown>> = {
   nextState: TState
   state?: TState
 }
-export type SetMind<TState extends Record<PropertyKey, unknown>> = (
-  patch: Patch<TState>,
-  config?: Parameters<SetState<TState>>['1'] & { notify?: boolean }
-) => void
--- src/logic/getPlugins.logic.ts
+++ src/logic/getPlugins.logic.ts
@@ -1,18 +1,1 @@
-import { watch } from '../helpers/helpers'
-import type { StateMap } from '../factory.types'
-
-type Store<TState extends Record<PropertyKey, unknown>> = {
-  notify: (nextState: TState, state?: TState) => void
-}
-
-export const getPlugins = <TState extends Record<PropertyKey, unknown>>(
-  store: Store<TState>
-) => ({
-  watch({ nextState, state }: StateMap<TState>) {
-    const modifiedNextState = watch(nextState, () => {
-      store.notify(nextState, state)
-    })
-
-    return { nextState: modifiedNextState, state: state }
-  },
-})
+export const getPlugins = (store: unknown) => ({})
